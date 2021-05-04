3e1814ac1fdd34e34fbbe887021dc4ed057f8bea
Don Jose Mathew <59858506+dontech09@users.noreply.github.com>
2021-05-04 16:51:58 +0530

    Store Data in Firebase

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,11 +1,65 @@
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
+import NewDisney from "./NewDisney.component";
+import Orginals from "./orginals.component";
+import Recommends from "./Recommends.component";
+import Trending from "./Trending.component";
 import Viewers from "./Viewers";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../features/movie/movieSlice";
+import { selectUserName } from "../features/user/userSlice";
 const Home = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(() => {
+    db.collection("movies").onSnapshot((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        switch (doc.data().type) {
+          case "recommend":
+            recommends = [...recommends, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "new":
+            newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "original":
+            originals = [...originals, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "trending":
+            trending = [...trending, { id: doc.id, ...doc.data() }];
+            break;
+          default:
+            return 0;
+        }
+      });
+    });
+
+    dispatch(
+      setMovies({
+        recommend: recommends,
+        NewDisney: newDisneys,
+        original: originals,
+        trending: trending,
+      })
+    );
+  }, [userName]);
   return (
     <Container>
       <ImgSlider />
       <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Orginals />
+      <Trending />
     </Container>
   );
 };
---
+++ src/features/movie/movieSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  recommeded: null,
+  newDisney: null,
+  orginal: null,
+  trending: null,
+};
+
+const movieSlice = createSlice({
+  name: "movie",
+  initialState,
+  reducers: {
+    setMovies: (state, action) => {
+      state.recommeded = action.payload.recommeded;
+      state.newDisney = action.payload.newDisney;
+      state.original = action.payload.original;
+      state.trending = action.payload.trending;
+    },
+  },
+});
+
+export const { setMovies } = movieSlice.actions;
+
+export const selectRecommend = (state) => state.movie.recommeded;
+export const selectNewDisney = (state) => state.movie.newDisney;
+export const selectOriginal = (state) => state.movie.original;
+export const selectTrending = (state) => state.movie.trending;
+
+export default movieSlice.reducer;
